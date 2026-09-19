674fce8f47e6f03a3f55aafa0e1536350365e956
emerald067 <eawoke@byupathway.edu>
2026-09-19 01:21:10 +0100

    Configure Swagger for deployment environments

--- cse341-api/swagger.js
+++ cse341-api/swagger.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
@@ -5,11 +7,13 @@ const doc = {
         title: "Contacts API",
         description: "API for managing contacts"
     },
-    host: "localhost:8080",
-    basePath: "/contacts"
+    host: process.env.SWAGGER_HOST || "localhost:8080",
+    basePath: "/contacts",
+    schemes: [process.env.SWAGGER_SCHEME || "http"]
 };
 
 const outputFile = "./swagger.json";
+
 const endpointsFiles = ["./routes/contacts.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
